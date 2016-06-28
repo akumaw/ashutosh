b1b3c99a6e903d56b5ca6a93d1bbf38593aa8165
akumaw <akumaw@NOILAB66149.sapient.com>
2016-06-28 12:03:23 +0530

    1st java commit

---
+++ OverrideDemo/src/com/sapient/client/ClientA.java
@@ -0,0 +1,14 @@
+package com.sapient.client;
+
+import com.sapient.beans.NokiaLumia;
+
+public class ClientA {
+
+	public static void main (String[] args) {
+		NokiaLumia obj = new NokiaLumia();
+		obj.doConverse();
+		obj.sendSms();
+		obj.do2gconverse();
+		obj.captureVideo();
+	}
+}
